1d07b8898e5082b461a95cfa0d1d8dbfb86ed8ce
Mariusz Nowak <mariusz@alphasoc.com>
2020-11-24 13:28:35 +0100

    Tidy up main.go

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/ioj/sqlty/compiler"
 	"github.com/ioj/sqlty/db"
@@ -11,81 +13,58 @@ import (
 	"github.com/ioj/sqlty/stmt"
 )
 
-func main() {
-	// queries, err := compiler.CompileFile("compiler/_queries/select_1.sql")
-	queries, err := compiler.CompileString("q", `
-		/*
-			@name foobar_by_id
-			@notNullParams id
-			@many
-		*/
-		SELECT * FROM test2 WHERE id = :id;
-
-		/*
-			@name foobar_by_col
-			@param timestamps ((ts1, ts2)...)
-			@exec
-		*/
-		INSERT INTO test2 (ts1, ts2) VALUES :timestamps;
-	`)
+func compiledir(connString, dirPath string) error {
+	sqlFiles, err := filepath.Glob(path.Join(dirPath, "*.sql"))
 	if err != nil {
-		if perr, ok := err.(*compiler.ErrCompilationFailed); ok {
-			for _, err := range perr.Errors {
-				fmt.Println(err.Sprintf())
-			}
-			return
-		}
-
-		log.Fatal(err)
+		return err
 	}
 
-	resolver, err := db.NewResolver(context.Background(), "postgres://localhost/sqlty_test")
+	ctx := context.Background()
+	resolver, err := db.NewResolver(ctx, connString)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer resolver.Close()
 
-	stmtqueries := &stmt.Queries{PackageName: "sql"}
-
-	for _, q := range queries {
-		fmt.Println(q.PreparedQuery())
-		params, returnvals, err := resolver.ResolveTypes(context.Background(),
-			q.PreparedQuery(), q.NotNullArray())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for n, p := range params {
-			fmt.Printf("$%v: %+v\n", n+1, p)
-		}
-
-		fmt.Println("---")
-
-		for _, rv := range returnvals {
-			fmt.Printf("%v: %+v\n", rv.Name, rv.Type)
-		}
-
-		stmtq, err := q.StmtQuery(params, returnvals)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%+v\n", *stmtq)
-
-		stmtqueries.Queries = append(stmtqueries.Queries, stmtq)
-	}
-
 	gen, err := generator.New("./templates/*.go.tpl")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	enums := &stmt.Enums{PackageName: "sql", Enums: resolver.Enums()}
-	if err := gen.Enums("/home/ioj/projects/sqlty-gen/sql", enums); err != nil {
-		log.Fatal(err)
+	if err := gen.Enums(dirPath, enums); err != nil {
+		return err
 	}
 
-	if err := gen.Queries("/home/ioj/projects/sqlty-gen/sql/generated.go", stmtqueries); err != nil {
+	for _, fname := range sqlFiles {
+		q, err := compiler.CompileFile(fname)
+		if err != nil {
+			return err
+		}
+
+		params, returnvals, err := resolver.ResolveTypes(ctx, q.PreparedQuery(), q.NotNullArray())
+		if err != nil {
+			return err
+		}
+
+		stmtq, err := q.StmtQuery("sql", params, returnvals)
+		if err != nil {
+			return err
+		}
+
+		fnameOut := strings.TrimRight(fname, path.Ext(fname)) + ".gen.go"
+		if err := gen.Query(fnameOut, stmtq); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	connString := "postgres://localhost/sqlty_test"
+	dirPath := "/home/ioj/projects/sqlty-gen/sql"
+	if err := compiledir(connString, dirPath); err != nil {
 		log.Fatal(err)
 	}
 }
